51e1d7a08f054bf3aaf2b94f5d8ba3a5fa0ec549
miskiaino <vilanordakill@gmail.com>
2018-11-16 16:44:37 +0500

    fwefew

--- tgbot.py
+++ tgbot.py
@@ -8,6 +8,21 @@ TOKEN = '721378927:AAF3OX-i_oXfK0asbUJzDi5JhVVujShLomI'
 bot = telebot.TeleBot(TOKEN)
 USERS = set()
 
+
+# удаление сообщение с ссылками
+GROUP_ID = -1001320869736  # Ваш ID группы
+
+@bot.message_handler(func=lambda message: message.entities is not None and message.chat.id == GROUP_ID)
+def delete_links(message):
+    for entity in message.entities:  # Пройдёмся по всем entities в поисках ссылок
+        # url - обычная ссылка, text_link - ссылка, скрытая под текстом
+        if entity.type in ["url", "text_link"]:
+            # Мы можем не проверять chat.id, он проверяется ещё в хэндлере
+            bot.delete_message(message.chat.id, message.message_id)
+        else:
+            return
+
+
 @bot.message_handler(commands=['start','help'])
 def command_handler(message: Message):
     bot.reply_to(message, 'hello, im is helpless bot')
@@ -26,18 +41,6 @@ def echo_digits(message: Message):
 def sticker_handler(message: Message):
     bot.reply_to(message, 'опять стикеры с оняме')
 
-# удаление сообщение с ссылками
-GROUP_ID = -1001320869736  # Ваш ID группы
-
-@bot.message_handler(func=lambda message: message.entities is not None and message.chat.id == GROUP_ID)
-def delete_links(message):
-    for entity in message.entities:  # Пройдёмся по всем entities в поисках ссылок
-        # url - обычная ссылка, text_link - ссылка, скрытая под текстом
-        if entity.type in ["url", "text_link"]:
-            # Мы можем не проверять chat.id, он проверяется ещё в хэндлере
-            bot.delete_message(message.chat.id, message.message_id)
-        else:
-            return
 
 
 bot.polling(none_stop=True)
