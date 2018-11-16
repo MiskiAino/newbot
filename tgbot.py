d54478b37b4c7d1f087c64f822dd0a02895d7a46
miskiaino <vilanordakill@gmail.com>
2018-11-16 16:47:11 +0500

    fwefew

--- tgbot.py
+++ tgbot.py
@@ -16,30 +16,30 @@ GROUP_ID = -1001320869736  # Ваш ID группы
 def delete_links(message):
     for entity in message.entities:  # Пройдёмся по всем entities в поисках ссылок
         # url - обычная ссылка, text_link - ссылка, скрытая под текстом
-        if entity.type in ["url", "text_link"]:
+        if entity.type in ["url","" "text_link"]:
             # Мы можем не проверять chat.id, он проверяется ещё в хэндлере
             bot.delete_message(message.chat.id, message.message_id)
         else:
             return
 
 
-@bot.message_handler(commands=['start','help'])
-def command_handler(message: Message):
-    bot.reply_to(message, 'hello, im is helpless bot')
-
-@bot.message_handler(content_types=['text'])
-def echo_digits(message: Message):
-    reply = str(random.random())
-
-    if message.from_user.id in USERS:
-        reply = f"{message.from_user.username}, hello again"
-
-    bot.reply_to(message, reply)
-    USERS.add(message.from_user.id)
-
-@bot.message_handler(content_types=['sticker'])
-def sticker_handler(message: Message):
-    bot.reply_to(message, 'опять стикеры с оняме')
+# @bot.message_handler(commands=['start','help'])
+# def command_handler(message: Message):
+#     bot.reply_to(message, 'hello, im is helpless bot')
+#
+# @bot.message_handler(content_types=['text'])
+# def echo_digits(message: Message):
+#     reply = str(random.random())
+#
+#     if message.from_user.id in USERS:
+#         reply = f"{message.from_user.username}, hello again"
+#
+#     bot.reply_to(message, reply)
+#     USERS.add(message.from_user.id)
+#
+# @bot.message_handler(content_types=['sticker'])
+# def sticker_handler(message: Message):
+#     bot.reply_to(message, 'опять стикеры с оняме')
 
 
 
